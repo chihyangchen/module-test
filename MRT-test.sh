c7b9cd658357426d615de9b39e4dda0a599fe6b2
Young CY Chen (陳智揚) <youngcy.chen@moxa.com>
2020-11-02 05:19:32 +0000

    update the clock & QENG

--- MRT-test.sh
+++ MRT-test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+device='/dev/ttyUSB2'
+cat "$device" > log &
+sleep 0.5
+echo 'ATE0' >> "$device"
+#test(){
+for i in {1..5}
+do
+	echo 'at+qeng="servingcell"' >> "$device"
+	sleep 0.5
+	echo 'at+cclk?' >> "$device"
+	sleep 0.5
+done
 
-cat /dev/$1 > $2 &
-echo 'at+cclk?' > /dev/$1
 killall cat
+cat log
+#}
+
+
+
+
