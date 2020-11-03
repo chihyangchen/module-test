aaf7076211c7640da4b183fc09a70aa8b72c69dc
Young CY Chen (陳智揚) <YoungCY_Chen@moxa.com>
2020-11-03 19:28:18 +0800

    checking

--- young-utilis.sh
+++ young-utilis.sh
@@ -18,4 +18,44 @@ delip(){
 	echo "IP route delete Done"
 }
 
+# Connect by the qmi tool
+
+connect()
+{
+	delip
+	cd /home/work/
+	./connect-5g-by-qmi.sh
+}
+linkdown()
+{
+	ifconfig wwan0 down
+	moxaip
+}
+
+
+# here to build and mount a ramdisk 
+buildramdisk()
+{
+	mkdir /tmp/ramdisk
+	chmod 777 /tmp/ramdisk
+	mount -t tmpfs -o size=2G tmpfs /tmp/ramdisk
+}
+
+check()
+{
+	numoffile=`find /tmp/ramdisk -maxdepth 1 -type f | wc -l`
+	echo "$numoffile"
+	filename=`ls /tmp/ramdisk -t | tail -1`
+       	echo "$filename"
+	curdir=$PWD
+	echo "$curdir"
+	mv /tmp/ramdisk/"$filename" "$curdir"/"$filename"
+	echo "$?"	
+}
+#function
+
 $1
+
+
+
+
